4bfdbee87beb4ff7e454f538104b500976550b38
nikhat29 <nikki.ali2905@gmail.com>
2025-01-25 14:42:04 +0530

    fix warnings

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,16 +1,14 @@
 import React from 'react'
 
 const Question = ({data, onAnswer, currentQuestionIndex, totalQuestions, displayHints, onHintClick, answered, timeLeft,selectedAnswer}) => {
-    const handleHints = () => {
-        data.hints ? (<p>Hint: {data.hints}</p>) : (<p>No help available for this question</p>)
-    }
+   
   return (
     <div>
        <div className='question-timer'> <h2>Question {currentQuestionIndex+1} / {totalQuestions}</h2>
        <span>{timeLeft} seconds</span></div>
         <div className='question-container'>
         <p className='question'>{data.question}</p>
-       <div className='img-container'> {data.image && <img src={data.image} className='image' width="50%"/>}</div>
+       <div className='img-container'> {data.image && <img src={data.image} className='image' width="50%" alt={data.question}/>}</div>
         <div className='option-container'>{
             data.options.map((option, index)=>(
                   <button key={index} onClick={()=> onAnswer(option)} disabled={answered}
